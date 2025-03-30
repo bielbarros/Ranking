f32d42877a54587f24ffb44489a50521b4c45315
bielbarros <gabrielbarrosg11@gmail.com>
2025-03-30 09:32:12 -0300

    feat: Creating the Controller (FuncionarioController) This class exposes the REST API.

---
+++ ranking/src/main/java/com/ranking/ranking/controller/FuncionarioController.java
@@ -0,0 +1,36 @@
+package com.ranking.ranking.controller;
+
+import com.ranking.ranking.entity.Funcionario;
+import com.ranking.ranking.service.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/funcionarios")
+public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    // Adicionar um novo funcionário
+    @PostMapping("/criar")
+    public Funcionario criarFuncionario(@RequestBody Funcionario funcionario) {
+        return funcionarioService.criarFuncionario(funcionario);
+    }
+
+
+    // Listar ranking dos funcionários
+    @GetMapping("/ranking")
+    public List<Funcionario> listarRanking() {
+        return funcionarioService.listarRanking();
+    }
+
+    // Adicionar pontos a um funcionário
+    @PostMapping("/{id}/adicionar-pontos")
+    public Funcionario adicionarPontos(@PathVariable Long id, @RequestParam int pontos) {
+        return funcionarioService.adicionarPontos(id, pontos);
+    }
+
+
+}

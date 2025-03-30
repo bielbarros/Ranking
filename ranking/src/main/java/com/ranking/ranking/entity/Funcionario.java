57b85c80aeb4b86184ca13f6f6081abd95d467dc
bielbarros <gabrielbarrosg11@gmail.com>
2025-03-30 09:36:48 -0300

    feat: Adding Getter and Setter methods to the Employee class.

--- ranking/src/main/java/com/ranking/ranking/entity/Funcionario.java
+++ ranking/src/main/java/com/ranking/ranking/entity/Funcionario.java
@@ -16,4 +16,27 @@ public class Funcionario {
     private String nome;
     private int pontos;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
 }

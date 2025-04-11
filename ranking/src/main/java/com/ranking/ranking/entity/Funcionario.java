eca760a9857689025d93830f44ab1599e8b48441
bielbarros <gabrielbarrosg11@gmail.com>
2025-04-11 19:03:39 -0300

    feat: Refactor entity funcionario.java with lombok.

--- ranking/src/main/java/com/ranking/ranking/entity/Funcionario.java
+++ ranking/src/main/java/com/ranking/ranking/entity/Funcionario.java
@@ -3,10 +3,14 @@ package com.ranking.ranking.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Funcionario {
 
     @Id
@@ -16,36 +20,4 @@ public class Funcionario {
     private String nome;
     private int pontos;
     private String meta;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getPontos() {
-        return pontos;
-    }
-
-    public void setPontos(int pontos) {
-        this.pontos = pontos;
-    }
-
-    public String getMeta() {
-        return meta;
-    }
-
-    public void setMeta(String meta) {
-        this.meta = meta;
-    }
 }
